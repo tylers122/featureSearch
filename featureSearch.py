fddf253fa877674c4cf232a8bc460b133b9cfc70
Tyler See <72323540+tylers122@users.noreply.github.com>
2023-03-08 18:14:50 -0800

    First commit

---
+++ featureSearch.py
@@ -0,0 +1,18 @@
+import time
+
+def main():
+    print("Welcome to Tyler See's Feature Selection Algorithm!")
+
+    num = input("Please enter total number of features: ")
+    print()
+
+    print("Type the number of the algorithm you want to run.")
+    print("-Forward Selection")
+    print("-Backward Elimination")
+    alg = input()
+
+def forward():
+    pass
+
+def backward():
+    pass
