7a52666f52daecc057e2ec3bfd4f414f16313097
Tyler See <72323540+tylers122@users.noreply.github.com>
2023-03-08 20:30:31 -0800

    Added forward selection
    Added forward selection
    Using random numbers for accuracy

--- featureSearch.py
+++ featureSearch.py
@@ -1,18 +1,58 @@
+import numpy as np
 import time
+import random
 
 def main():
-    print("Welcome to Tyler See's Feature Selection Algorithm!")
+    # print("Welcome to Tyler See's Feature Selection Algorithm!")
 
-    num = input("Please enter total number of features: ")
-    print()
+    # num = input("Please enter total number of features: ")
+    # print()
 
-    print("Type the number of the algorithm you want to run.")
-    print("-Forward Selection")
-    print("-Backward Elimination")
-    alg = input()
+    # print("Type the number of the algorithm you want to run.")
+    # print("-Forward Selection")
+    # print("-Backward Elimination")
+    # alg = input()
+
+    data = readFile()
+    forward()
+
+def readFile():
+    name = input("Input file name: ")
+
+    data = np.loadtxt(name)
+    return data   
 
 def forward():
-    pass
+    currFeat = []
+    solution = []
+    bestAcc = 0
+    
+
+    for i in range(1, 5):
+        addFeat = 0
+        currBestAcc = 0
+        for j in range(1, 5):
+            if j not in currFeat:
+                accuracy = random.randint(0, 1000) / 10
+                print("Using feature(s)", str(currFeat + [j]), "accuracy is", str(accuracy) + "%")
+                if accuracy > currBestAcc:
+                    currBestAcc = accuracy
+                    addFeat = j
+        
+        if currBestAcc < bestAcc:
+            print("Warning, Accuracy has decreased!")
+            break
+        bestAcc = currBestAcc
+        solution.append(addFeat)
+        currFeat.append(addFeat)
+        print("Feature set", str(currFeat), "was best, accuracy is", str(currBestAcc) + "%")
+        print()
+
+    print("Finished search! The best feature subset is", str(solution), "which has an accuracy of", str(bestAcc) + "%")
+
+    
 
 def backward():
     pass
+
+main()
